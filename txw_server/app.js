4461d03c54c9ed877496f1652e9fea8a07157a09
web1982 <yo196820906@qq.com>
2019-06-04 20:14:36 +0800

    添加服务器项目

---
+++ txw_server/app.js
@@ -0,0 +1,38 @@
+
+//1:引入模块  express mysql
+const express = require("express");
+const mysql = require("mysql");
+//2:创建连接池
+var pool = mysql.createPool({
+  host:"127.0.0.1",
+  user:"root",
+  password:"",
+  port:3306,
+  database:"txw",
+  connectionLimit:15
+})
+//3:创建express对象
+var server = express();
+
+//4:指定静态资源(图片应该放在服务器上)
+server.use(express.static("public"));
+
+//5:处理跨域请求
+const cors = require("cors");
+server.use(cors({
+  origin:["http://127.0.0.1:8080",
+  "http://localhost:8080"],
+  credentials:true
+}));
+//6:添加session功能
+const session = require("express-session");
+server.use(session({
+  secret:"128位字符串",
+  resave:true,
+  saveUninitialized:true
+}));
+//9:绑定监听端口
+server.listen(3000);
+
+//10：接口部分
+
