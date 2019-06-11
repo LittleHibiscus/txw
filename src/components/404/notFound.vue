c875c96b8b63072c0c04c355b64e01ba836f59da
ZhaoRui <18954500365@189.cn>
2019-06-11 10:02:55 +0800

    优化目录结构 完成 404 页面

---
+++ src/components/404/notFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="wrap ac" style="overflow:hidden">
+      <img :src="require('@/assets/404.gif')" style="margin-bottom:20px;">
+      <div class="cboth"></div>
+      <button-link title="天巡主页"></button-link>
+      <div class="mgb7"></div>
+    </div>
+  </div>
+</template>
+<script>
+import buttonLink from './buttonLink.vue'
+export default {
+  components:{buttonLink}
+};
+</script>
+<style scoped>
+</style>
